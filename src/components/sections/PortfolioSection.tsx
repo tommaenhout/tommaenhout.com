c232927709e87bea0a17c1f2c605592d669cd1cf
tommaenhout <tom.maenhout@gmail.com>
2024-07-05 18:30:34 -0300

    adjust slider

--- src/components/sections/PortfolioSection.tsx
+++ src/components/sections/PortfolioSection.tsx
@@ -3,32 +3,31 @@ import 'react-awesome-slider/dist/styles.css';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import "./styles.css";
  
-const slider = (
-  <AwesomeSlider
-    className='aws-btn'
-    animation="cubeAnimation"
-    mobileTouch={true}
-    bullets={false}
-    media={[
-      {
-        source: '/images/stranger-things-5.jpg',
-      },
-      {
-        source: '/images/stranger-things-7.png',
-      },
-      {
-        source: '/images/lwork3.jpg',
-      },
-    ]}
-  />
-);
+
 const PortFolioSection = () => {
+  
+    const onClickHandler = (e : React.MouseEvent<HTMLElement> ) =>{
+        console.log(e.target)
+    };
+
+ 
     return (
       <div className="content">
         {/* title */}
         <div className="title py-3">Portfolio</div>
         {/* content */}
-        <div className='bg-black mt-7 max-h-[80%] relative'>{slider}</div>
+        <div className='mt-7 max-h-[80%] relative'>
+        <AwesomeSlider
+        className='aws-btn'
+        animation="cubeAnimation"
+        mobileTouch={true}
+        bullets={false}
+        >
+        <div onClick={onClickHandler} data-src="https://www.w3schools.com/w3images/nature.jpg" />
+        <div data-src="https://www.w3schools.com/w3images/snow.jpg" />
+        <div data-src="https://www.w3schools.com/w3images/mountains.jpg" />
+        </AwesomeSlider>
+        </div>
         <div className='border-b-4 h-10'></div>
       </div>
     );
