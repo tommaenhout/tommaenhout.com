af39a68269cc0b3a7d2057978e9ac9609d751e27
tommaenhout <tom.maenhout@gmail.com>
2024-07-04 21:40:33 -0300

    add slider and styling

--- src/components/sections/PortfolioSection.tsx
+++ src/components/sections/PortfolioSection.tsx
@@ -1,14 +1,20 @@
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import "./styles.css";
  
 const slider = (
   <AwesomeSlider
+    className='aws-btn'
+    animation="cubeAnimation"
+    mobileTouch={true}
+    bullets={false}
     media={[
       {
-        source: '/images/lwork1.jpg',
+        source: '/images/stranger-things-5.jpg',
       },
       {
-        source: '/images/lwork2.jpg',
+        source: '/images/stranger-things-7.png',
       },
       {
         source: '/images/lwork3.jpg',
@@ -22,8 +28,8 @@ const PortFolioSection = () => {
         {/* title */}
         <div className="title py-3">Portfolio</div>
         {/* content */}
-        <div className='bg-black max-h-[80%] relative p-2'>{slider}</div>
-        <div></div>
+        <div className='bg-black mt-7 max-h-[80%] relative'>{slider}</div>
+        <div className='border-b-4 h-10'></div>
       </div>
     );
   };
