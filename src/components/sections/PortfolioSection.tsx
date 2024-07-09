d86603f5c8dfcb45adcb95b16b3a4653e185c8f5
tommaenhout <tom.maenhout@gmail.com>
2024-07-09 18:57:01 -0300

    add pictures to portfolio

--- src/components/sections/PortfolioSection.tsx
+++ src/components/sections/PortfolioSection.tsx
@@ -1,13 +1,15 @@
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import robinson from '../../../public/images/robinson.png';
+import hype from '../../../public/images/hype.png';
 import "./styles.css";
  
 
 const PortFolioSection = () => {
   
-    const onClickHandler = (e : React.MouseEvent<HTMLElement> ) =>{
-        console.log(e.target)
+    const onClickHandler = ( link : string ) =>{
+       window.open(link, '_blank');
     };
 
  
@@ -17,18 +19,18 @@ const PortFolioSection = () => {
         <div className="title py-3">Portfolio</div>
         {/* content */}
         <div className='mt-7 max-h-[80%] relative'>
-        <AwesomeSlider
-        className='aws-btn'
-        animation="cubeAnimation"
-        mobileTouch={true}
-        bullets={false}
-        >
-        <div onClick={onClickHandler} data-src="https://www.w3schools.com/w3images/nature.jpg" />
-        <div data-src="https://www.w3schools.com/w3images/snow.jpg" />
-        <div data-src="https://www.w3schools.com/w3images/mountains.jpg" />
-        </AwesomeSlider>
-        </div>
-        <div className='border-b-4 h-10'></div>
+          <div className='absolute z-10 h-full w-full flex justify-center items-center text-black'>Robinsons institute</div>
+          <AwesomeSlider
+          onTransitionEnd={(props) => console.log(props.currentIndex)}
+          className='aws-btn'
+          animation="cubeAnimation"
+          mobileTouch={true}
+          bullets={false}
+          >
+          <div onClick={() => onClickHandler ("https://robinsonsinstitute.com/")} data-src= {robinson.src}/>
+          <div onClick={() => onClickHandler ("https://hype-ba.com/")} data-src= {hype.src} />
+          </AwesomeSlider>
+      </div>
       </div>
     );
   };
