b20d7a9414878baba1f79dc09cb78a000145a191
tommaenhout <tom.maenhout@gmail.com>
2024-07-13 09:59:32 -0300

    setting up email service

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -1,4 +1,42 @@
-const ContactForm = () => {
+
+import React, { useRef } from 'react';
+import emailjs from "@emailjs/browser";
+
+const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID
+const templateId = process.env.NEXT_PUBLIC_TEMPLATE_ID
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY
+
+
+
+const ContactForm =  () => {
+  const ref = useRef<HTMLFormElement>(null);
+  console.log(serviceId, templateId, publicKey)
+
+  const onsubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = ref.current;
+    if (!form) return;
+    const values = new FormData(form);
+    const data = {
+      name: values.get("name"),
+      email: values.get("email"),
+      message: values.get("message"),
+    };
+
+    if (serviceId === undefined || templateId === undefined || publicKey === undefined) {
+      console.error(serviceId, templateId, publicKey);
+      return;
+    }
+    emailjs.sendForm(serviceId, templateId, form, publicKey)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+  
+
+
   return (
     <div className="content contacts">
       {/* title */}
@@ -7,7 +45,7 @@ const ContactForm = () => {
       <div className="row">
         <div className="col col-d-12 col-t-12 col-m-12 border-line-v">
           <div className="contact_form">
-            <form id="cform" method="post">
+            <form onSubmit={onsubmitHandler} ref={ref} id="cform" method="post">
               <div className="row">
                 <div className="col col-d-6 col-t-6 col-m-12">
                   <div className="group-val">
@@ -34,10 +72,10 @@ const ContactForm = () => {
                 </div>
               </div>
               <div className="align-left">
-                <a href="#" className="button">
+                <button  type='submit' className="button">
                   <span className="text">Send Message</span>
                   <span className="arrow" />
-                </a>
+                </button>
               </div>
             </form>
             <div className="alert-success">
